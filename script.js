799505972eeb1f046877b079efef19c4e6f8b632
Orazgeldi Orazgylyjov <orazgeldiorazgylyjov@Orazgeldis-MacBook-Pro.local>
2022-12-06 16:41:37 +0200

    adding loader

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
+const quoteContainer = document.getElementById('q-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
 let apiQuotes = [];
+
+function loading () {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+function complete () {
+  quoteContainer.hidden = false;
+  loader.hidden = true;
+}
 
 const newQuote = () => {
   const quoteNumber = apiQuotes[Math.floor(Math.random() * apiQuotes.length)]
-  console.log(quoteNumber);
+  if(!quoteNumber.author) quoteText.textContent = 'Unknown';
+   else quoteText.textContent = quoteNumber.text;
+  
+  if(quoteNumber.text.length > 60 ) quoteText.classList.add('long-quote')
+    else quoteText.classList.remove('long-quote');
+  authorText.textContent = quoteNumber.author;
 }
 
 async function getQuotes (){
@@ -16,4 +38,20 @@ async function getQuotes (){
   }
 }
 
-getQuotes();
+// function tweetQuote () {
+//   const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+//   window.open(twitterUrl, '_blank');
+// }
+
+function tweetQuote() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.innerText} - ${authorText.innerText}`;
+  window.open(twitterUrl, '_blank');
+}
+
+// Event Listeners
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+
+
+// getQuotes();
+loading();
